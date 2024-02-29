20e1af7078f1bb6bb870e8807c73d21199130178
mentix02 <manan.yadav02@gmail.com>
2024-02-29 17:46:20 +0530

    Some changes

--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,51 @@ import { fileURLToPath, URL } from "node:url";
 
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
+import { VitePWA } from "vite-plugin-pwa";
 import vuetify from "vite-plugin-vuetify";
 import vueJsx from "@vitejs/plugin-vue-jsx";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueJsx(), vuetify({ autoImport: true })],
+  plugins: [
+    vue(),
+    vueJsx(),
+    vuetify({ autoImport: true }),
+    VitePWA({
+      registerType: "autoUpdate",
+      manifest: {
+        theme_color: "#E91E63",
+        background_color: "#FFFFFF",
+        icons: [
+          {
+            src: "/android-chrome-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "/android-chrome-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+          {
+            src: "/apple-touch-icon.png",
+            sizes: "180x180",
+            type: "image/png",
+          },
+          {
+            src: "/favicon-16x16.png",
+            sizes: "16x16",
+            type: "image/png",
+          },
+          {
+            src: "/favicon-32x32.png",
+            sizes: "32x32",
+            type: "image/png",
+          },
+        ],
+      },
+    }),
+  ],
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
